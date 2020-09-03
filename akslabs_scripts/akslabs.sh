b1dd5e0ba7782643032b8b93d5d5fc65757bc5fc
Sergio Turrent <seturren@microsoft.com>
2020-09-03 07:11:19 -0600

    Updated version id

--- akslabs_scripts/akslabs.sh
+++ akslabs_scripts/akslabs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # script name: akslabs.sh
-# Version v0.1.9 20200731
+# Version v0.1.10 20200903
 # Set of tools to deploy AKS troubleshooting labs
 
 # "-l|--lab" Lab scenario to deploy (5 possible options)
@@ -58,7 +58,7 @@ done
 # Variable definition
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SCRIPT_NAME="$(echo $0 | sed 's|\.\/||g')"
-SCRIPT_VERSION="Version v0.1.9 20200731"
+SCRIPT_VERSION="Version v0.1.10 20200903"
 
 # Funtion definition
 
